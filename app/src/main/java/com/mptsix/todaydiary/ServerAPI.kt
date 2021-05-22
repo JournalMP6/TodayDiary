dede7a7968a662b15507859cd1d64a2ea99fae51
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-05-22 17:29:40 +0900

    Model[1] : retrofit object 선언 및 serverAPI 선언

---
+++ app/src/main/java/com/mptsix/todaydiary/ServerAPI.kt
@@ -0,0 +1,16 @@
+package com.mptsix.todaydiary
+
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface ServerAPI {
+    @FormUrlEncoded
+    @POST("/api/v1/login")
+    fun login(@Field("loginRequest") loginRequest: LoginRequest): Call<LoginResponse>
+
+    @FormUrlEncoded
+    @POST("/api/v1/user")
+    fun registerUser(@Field("userRegisterRequest") userRegisterRequest: UserRegisterRequest):Call<UserRegisterResponse>
+}
---
+++ app/src/main/java/com/mptsix/todaydiary/ServerRepository.kt
@@ -0,0 +1,26 @@
+package com.mptsix.todaydiary
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ServerRepository {
+    private var instance:Retrofit ?= null
+    private val gson = GsonBuilder().setLenient().create()
+    private const val URL = "http://www.naver.com"
+
+    public fun getInstance():Retrofit{
+        if(instance == null) {
+            instance = Retrofit.Builder()
+                .baseUrl(URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+        return instance!!
+    }
+
+//    private fun initApiInterface():ServerAPI{
+//        return retroFit.create()
+//    }
+}
