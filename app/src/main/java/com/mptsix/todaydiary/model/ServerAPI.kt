f0c2e8485da5a2293355b51526d35bb18cf938a1
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-05-27 00:52:57 +0900

    Model[3] : Made registerJournal, registerPicture, getJournal

--- app/src/main/java/com/mptsix/todaydiary/model/ServerAPI.kt
+++ app/src/main/java/com/mptsix/todaydiary/model/ServerAPI.kt
@@ -1,12 +1,15 @@
 package com.mptsix.todaydiary.model
 
+import com.mptsix.todaydiary.data.JournalDto
+import com.mptsix.todaydiary.data.JournalResponse
+import com.mptsix.todaydiary.data.PictureResponse
 import com.mptsix.todaydiary.data.request.LoginRequest
 import com.mptsix.todaydiary.data.request.UserRegisterRequest
 import com.mptsix.todaydiary.data.response.LoginResponse
 import com.mptsix.todaydiary.data.response.UserRegisterResponse
+import okhttp3.MultipartBody
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface ServerAPI {
     @POST("/api/v1/login")
@@ -14,4 +17,18 @@ interface ServerAPI {
 
     @POST("/api/v1/user")
     fun registerUser(@Body userRegisterRequest: UserRegisterRequest):Call<UserRegisterResponse>
+
+    @POST("/api/v1/journal")
+    fun registerJournal(@HeaderMap header:HashMap<String, Any?>,
+                        @Body journalDto: JournalDto
+    ): Call<JournalResponse>
+
+    @Multipart
+    @POST("/api/v1/journal/picture")
+    fun registerPicture(@HeaderMap header: HashMap<String, Any?>,
+                        @Part files: MultipartBody.Part
+    ):Call<PictureResponse>
+
+    @GET("/api/v1/journal")
+    fun getJournal(@HeaderMap header: HashMap<String, Any?>): Call<List<JournalDto>>
 }
