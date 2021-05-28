69ea9327963a162a04b7e7b4c3397fe9a3974ff3
fungi <yds05074@gmail.com>
2021-05-29 00:11:15 +0900

    finish calender activity

--- app/src/main/java/com/mptsix/todaydiary/model/ServerRepository.kt
+++ app/src/main/java/com/mptsix/todaydiary/model/ServerRepository.kt
@@ -12,7 +12,7 @@ object ServerRepository {
     private var instance: ServerAPI? = null
     private val serverApi: ServerAPI get() = instance!!
     private var userToken: String? = null
-    private const val URL = "http://192.168.0.46:8080"
+    private const val URL = "http://192.168.25.40:8080"
 
     init {
         getInstance()
--- app/src/main/java/com/mptsix/todaydiary/view/MainActivity.kt
+++ app/src/main/java/com/mptsix/todaydiary/view/MainActivity.kt
@@ -1,14 +1,34 @@
 package com.mptsix.todaydiary.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.mptsix.todaydiary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var getYear:String
+    lateinit var getMonth:String
+    lateinit var getDayOfMonth:String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        init()
     }
+
+    private fun init() {
+        binding.calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            getYear = year.toString()
+            getMonth = month.toString()
+            getDayOfMonth = dayOfMonth.toString()
+
+            intent = Intent(this, DiaryActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(applicationContext, getYear + "/" + getMonth + "/" + getDayOfMonth, Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
