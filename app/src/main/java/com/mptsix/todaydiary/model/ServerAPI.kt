1b3ed6f1af2de8017327dbee62f7d09a16dba60b
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-06-03 06:55:00 +0900

    Model[7] : Add profile interface at ServerAPI

--- app/src/main/java/com/mptsix/todaydiary/model/ServerAPI.kt
+++ app/src/main/java/com/mptsix/todaydiary/model/ServerAPI.kt
@@ -1,5 +1,7 @@
 package com.mptsix.todaydiary.model
 
+import com.mptsix.todaydiary.data.internal.PasswordChangeRequest
+import com.mptsix.todaydiary.data.internal.UserSealed
 import com.mptsix.todaydiary.data.request.LoginRequest
 import com.mptsix.todaydiary.data.request.UserRegisterRequest
 import com.mptsix.todaydiary.data.response.Journal
@@ -12,14 +14,23 @@ import retrofit2.http.*
 
 interface ServerAPI {
     @POST("/api/v1/login")
-    fun login(@Body loginRequest: LoginRequest): Call<LoginResponse>
+    fun login(@Body loginRequest: LoginRequest): Call<LoginResponse> // login
 
     @POST("/api/v1/user")
-    fun registerUser(@Body userRegisterRequest: UserRegisterRequest):Call<UserRegisterResponse>
+    fun registerUser(@Body userRegisterRequest: UserRegisterRequest):Call<UserRegisterResponse> // sign up
 
     @POST("/api/v1/journal")
-    fun registerJournal(@HeaderMap header:HashMap<String, Any?>, @Body journal: Journal): Call<JournalResponse>
+    fun registerJournal(@HeaderMap header:HashMap<String, Any?>, @Body journal: Journal): Call<JournalResponse> // regist journal
 
     @GET("/api/v1/journal/{time}")
-    fun getJournal(@HeaderMap header: HashMap<String, Any?>, @Path("time") journalTime: Long): Call<Journal>
+    fun getJournal(@HeaderMap header: HashMap<String, Any?>, @Path("time") journalTime: Long): Call<Journal> // get journal
+
+    @GET("/api/v1/user/sealed")
+    fun getSealedUser(@HeaderMap header: HashMap<String, Any?>):Call<UserSealed>
+
+    @PUT("/api/v1/user")
+    fun changePassword(@HeaderMap header: HashMap<String, Any?>, @Body passwordChangeRequest: PasswordChangeRequest): Call<ResponseBody>
+
+    @DELETE("/api/v1/user")
+    fun removeUser(@HeaderMap header: HashMap<String, Any?>):Call<ResponseBody>
 }
