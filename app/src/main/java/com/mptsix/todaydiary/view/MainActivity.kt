c8f50b59c6100150d46b7851031cfb7e37a22a4d
fungi <yds05074@gmail.com>
2021-06-04 22:09:43 +0900

    add UserInfoFragment & xml

--- app/src/main/java/com/mptsix/todaydiary/view/MainActivity.kt
+++ app/src/main/java/com/mptsix/todaydiary/view/MainActivity.kt
@@ -51,6 +51,10 @@ class MainActivity : AppCompatActivity() {
                     }
                     commitFragment(editDiaryFragment, true)
                 }
+
+                /*DisplayTransition.REQUSET_USERINFO->{
+                    commitFragment(userInfoFragment, true)
+                } -> UserInfoFragment open*/
             }
         }
     }
---
+++ app/src/main/java/com/mptsix/todaydiary/view/fragment/UserInfoFragment.kt
@@ -0,0 +1,18 @@
+package com.mptsix.todaydiary.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mptsix.todaydiary.R
+
+class UserInfoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_user_info, container, false)
+    }
+}
