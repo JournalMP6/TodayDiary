f842cc53c7c1394b9be1ed8d76126e2260c35345
KangDroid <hyunwoo286@gmail.com>
2021-06-07 19:12:00 +0900

    ViewModelHelper: Add some notes

--- app/src/main/java/com/mptsix/todaydiary/viewmodel/ViewModelHelper.kt
+++ app/src/main/java/com/mptsix/todaydiary/viewmodel/ViewModelHelper.kt
@@ -8,6 +8,16 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 open class ViewModelHelper: ViewModel() {
+
+    /**
+     * executeServeAndElse - Exchange data with server and handle both onSuccess and onFailure situation.
+     * Parameter:
+     * serverCallCore: () -> T --> The Server Execution Code[ServerRepository.*]
+     * onSuccess: suspend(successValue: T)->Unit --> When Server Execution is succeed[The result from serverCallCore() will be pushed to successValue]
+     * onFailure: suspend(failedThrowable: Throwable)->Unit --> When server execution is failed. Throwable will be pushed to failedThrowable
+     *
+     * Note: This function handles both viewModelScope and its context. Function callee do not have to use withContext or *.launch
+     */
     protected fun<T> executeServerAndElse(serverCallCore: () -> T, onSuccess: suspend(successValue: T)->Unit, onFailure: suspend(failedThrowable: Throwable)->Unit) {
         viewModelScope.launch {
             withContext(Dispatchers.IO) {
