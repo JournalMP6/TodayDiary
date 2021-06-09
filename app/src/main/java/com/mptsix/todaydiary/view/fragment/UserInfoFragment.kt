a00b3b81a1eda184940bfd38de8da5784256d2ba
KangDroid <hyunwoo286@gmail.com>
2021-06-09 18:29:14 +0900

    Fragment: Adapt SuperFragment to UserInfoFragment

--- app/src/main/java/com/mptsix/todaydiary/view/fragment/UserInfoFragment.kt
+++ app/src/main/java/com/mptsix/todaydiary/view/fragment/UserInfoFragment.kt
@@ -20,30 +20,26 @@ import com.mptsix.todaydiary.view.PwdChangeActivity
 import com.mptsix.todaydiary.viewmodel.ProfileViewModel
 import org.eazegraph.lib.models.PieModel
 
-class UserInfoFragment : Fragment() {
-    private var _fragmentUserInfoBinding: FragmentUserInfoBinding? = null
-    private val fragmentUserInfoBinding: FragmentUserInfoBinding get() = _fragmentUserInfoBinding!!
+class UserInfoFragment : SuperFragment<FragmentUserInfoBinding>() {
     private val profileViewModel: ProfileViewModel by activityViewModels()
 
     private val journalRVAdapter: JournalRVAdapter = JournalRVAdapter()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _fragmentUserInfoBinding = FragmentUserInfoBinding.inflate(inflater, container, false)
-        return fragmentUserInfoBinding.root
+    override fun getViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentUserInfoBinding {
+        return FragmentUserInfoBinding.inflate(inflater, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun initView() {
         changeUserPwd()
         deleteUser()
         followUser()
 
         // RecyclerView
-        fragmentUserInfoBinding.journalRecycler.adapter = journalRVAdapter
-        fragmentUserInfoBinding.journalRecycler.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        binding.journalRecycler.adapter = journalRVAdapter
+        binding.journalRecycler.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
 
         profileViewModel.userRemoveSucceed.observe(viewLifecycleOwner) {
             if (it) {
@@ -64,8 +60,8 @@ class UserInfoFragment : Fragment() {
         profileViewModel.sealedData.observe(viewLifecycleOwner) {
             if (it != null) {
                 // 가져온 유저 정보를 각각에 넣음
-                fragmentUserInfoBinding.userName.text = it.userName
-                fragmentUserInfoBinding.userEmail.text = it.userId
+                binding.userName.text = it.userName
+                binding.userEmail.text = it.userId
                 initPieChart(it.journalCategoryList)
                 journalRVAdapter.journalList = it.journalList
             }
@@ -74,20 +70,15 @@ class UserInfoFragment : Fragment() {
         profileViewModel.getSealedData()
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _fragmentUserInfoBinding = null
-    }
-
     private fun deleteUser(){
-        fragmentUserInfoBinding.deleteUser.setOnClickListener {
+        binding.deleteUser.setOnClickListener {
             profileViewModel.removeUser()
         }
     }// 버튼 클릭 시, 서버에서 유저 삭제
 
     private fun changeUserPwd(){
         lateinit var intent: Intent
-        fragmentUserInfoBinding.changePwd.setOnClickListener {
+        binding.changePwd.setOnClickListener {
             intent = Intent(activity, PwdChangeActivity::class.java)
             startActivity(intent)
         }
@@ -97,12 +88,12 @@ class UserInfoFragment : Fragment() {
         profileViewModel.getFollowingUser()
 
         profileViewModel.followingUserList.observe(viewLifecycleOwner){
-            fragmentUserInfoBinding.followNum.text = "Follower: " + it.size.toString()
+            binding.followNum.text = "Follower: " + it.size.toString()
         }
     }// follower 수를 보여줌
 
     private fun initPieChart(journalCategoryList: List<JournalCategoryResponse>){
-        fragmentUserInfoBinding.apply {
+        binding.apply {
             val colorList: List<Int> = listOf(
                 Color.parseColor("#FFA726"),
                 Color.parseColor("#66BB6A"),
