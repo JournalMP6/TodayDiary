de787ee77f89e69027841b400f10b364bf944eba
KangDroid <hyunwoo286@gmail.com>
2021-06-09 15:09:24 +0900

    View: if DB Data exists, auto-login

--- app/src/main/java/com/mptsix/todaydiary/view/splashActivity.kt
+++ app/src/main/java/com/mptsix/todaydiary/view/splashActivity.kt
@@ -3,26 +3,51 @@ package com.mptsix.todaydiary.view
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.mptsix.todaydiary.data.login.LoginSessionRepository
 import com.mptsix.todaydiary.databinding.ActivitySplashBinding
+import com.mptsix.todaydiary.viewmodel.SplashViewModel
+import java.lang.RuntimeException
+import java.net.ConnectException
+import java.net.SocketTimeoutException
 
 class splashActivity : AppCompatActivity() {
     lateinit var binding: ActivitySplashBinding
+    private val splashViewModel: SplashViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        LoginSessionRepository.initiateRepository(applicationContext)
         init()
+
+        // Splash Activity is the first activity shown to user.
+        LoginSessionRepository.initiateRepository(applicationContext)
+        splashViewModel.isLoginRedirectionNeeded {
+            when (it) {
+                is ConnectException, is SocketTimeoutException -> Toast.makeText(this, "서버와 연결을 하지 못했습니다. 인터넷 연결을 확인하고 재로그인 해주세요.", Toast.LENGTH_SHORT).show()
+                is RuntimeException -> Toast.makeText(this, "비밀번호가 외부에서 변경된 것 같습니다. 보안을 위해 자동 로그인 데이터를 모두 삭제합니다. 재로그인 해주세요.", Toast.LENGTH_SHORT).show()
+                else -> Toast.makeText(this, "알 수 없는 에러가 발생했습니다. 메시지: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
+
+            // Remove all user data
+            splashViewModel.removeAllSessionData()
+        }
     }
 
     private fun init() {
-        val handler = Handler()
-        handler.postDelayed({
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-        },3000)
+        splashViewModel.isDirectedToLogin.observe(this) {
+            if (it) {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                // login completed
+                val targetIntent: Intent = Intent(this, MainActivity::class.java)
+                startActivity(targetIntent)
+                finish()
+            }
+        }
     }// splash 화면, 3초동안 어플 정보를 띄워주고 로그인 페이지로 이동
 }
---
+++ app/src/main/java/com/mptsix/todaydiary/viewmodel/SplashViewModel.kt
@@ -0,0 +1,66 @@
+package com.mptsix.todaydiary.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mptsix.todaydiary.data.login.LoginSessionRepository
+import com.mptsix.todaydiary.data.request.LoginRequest
+import com.mptsix.todaydiary.model.ServerRepository
+import kotlinx.coroutines.*
+
+class SplashViewModel: ViewModel() {
+    var isDirectedToLogin: MutableLiveData<Boolean> = MutableLiveData()
+    private val loginSessionRepository: LoginSessionRepository by lazy {
+        LoginSessionRepository.getRepository()
+    }
+
+    fun isLoginRedirectionNeeded(onCommunicationFailure: (t: Throwable) -> Unit) {
+        val referenceStartTime: Long = System.currentTimeMillis()
+        viewModelScope.launch {
+            runCatching {
+                loginSessionRepository.getUserSession()
+            }.onFailure {
+                // it does not have session yet.
+                setLoginRedirection(true, referenceStartTime)
+            }.onSuccess {
+                withContext(Dispatchers.IO) {
+                    runCatching {
+                        ServerRepository.loginRequest(
+                            LoginRequest(
+                                userId = it.userId,
+                                userPassword = it.userPassword
+                            )
+                        )
+                    }.onFailure {
+                        Log.e(this::class.java.simpleName, "Failed to login: ${it.stackTraceToString()}")
+                        onCommunicationFailure(it)
+                        setLoginRedirection(true, referenceStartTime)
+                    }.onSuccess {
+                        setLoginRedirection(false, referenceStartTime)
+                    }
+                }
+            }
+        }
+    }
+
+    fun removeAllSessionData() {
+        viewModelScope.launch {
+            loginSessionRepository.removeAllEntries()
+        }
+    }
+
+    private suspend fun setLoginRedirection(targetValue: Boolean, referenceStartTime: Long) {
+        val endTime: Long = 3000 + referenceStartTime
+        val toSleep: Long = System.currentTimeMillis()
+        withContext(Dispatchers.IO) {
+            if (toSleep <= endTime) {
+                delay(endTime - toSleep)
+            }
+        }
+
+        withContext(Dispatchers.Main) {
+            isDirectedToLogin.value = targetValue
+        }
+    }
+}
