2e1913a594e58ea806a434479fa540b462bfa62f
KangDroid <hyunwoo286@gmail.com>
2021-06-09 18:27:13 +0900

    Fragment: Adapt SuperFragment to MainFragment

--- app/src/main/java/com/mptsix/todaydiary/view/fragment/MainFragment.kt
+++ app/src/main/java/com/mptsix/todaydiary/view/fragment/MainFragment.kt
@@ -1,54 +1,37 @@
 package com.mptsix.todaydiary.view.fragment
 
-import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.Fragment
 import com.mptsix.todaydiary.databinding.FragmentMainBinding
 import com.mptsix.todaydiary.viewmodel.JournalViewModel
 import java.text.SimpleDateFormat
 
-class MainFragment: Fragment() {
-    // For View Binding
-    private var _fragmentMainBinding: FragmentMainBinding? = null
-    private val fragmentMainBinding: FragmentMainBinding get() = _fragmentMainBinding!!
-
+class MainFragment: SuperFragment<FragmentMainBinding>() {
     // Simple Date Formatter
     private val simpleDateFormat: SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd")
 
     // Journal View Model[Context-Shared]
     private val journalViewModel: JournalViewModel by activityViewModels()
 
-    override fun onCreateView(
+    override fun getViewBinding(
         inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _fragmentMainBinding = FragmentMainBinding.inflate(inflater, container, false)
-        return fragmentMainBinding.root
+        container: ViewGroup?
+    ): FragmentMainBinding {
+        return FragmentMainBinding.inflate(inflater, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        fragmentMainBinding.calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+    override fun initView() {
+        binding.calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
             val currentTimeStamp: Long = simpleDateFormat.parse("${year}-${month+1}-$dayOfMonth").time
             journalViewModel.requestDiaryPage(currentTimeStamp)
         }
 
-        fragmentMainBinding.userInfoBtn.setOnClickListener {
+        binding.userInfoBtn.setOnClickListener {
             journalViewModel.requestUserInfoPage()
         }
-        fragmentMainBinding.userSearchBtn.setOnClickListener {
+        binding.userSearchBtn.setOnClickListener {
             journalViewModel.requestUserSearchPage()
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-
-        // Dereference variable so GC can free its memory
-        _fragmentMainBinding = null
     }
 }
