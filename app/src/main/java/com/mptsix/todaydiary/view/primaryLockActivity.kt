8fb39a9168080697d50d6e23394294a104f55fbc
fungi <yds05074@gmail.com>
2021-06-09 13:34:33 +0900

    add primaryLockActivity

--- app/src/main/java/com/mptsix/todaydiary/view/primaryLockActivity.kt
+++ app/src/main/java/com/mptsix/todaydiary/view/primaryLockActivity.kt
@@ -4,24 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.mptsix.todaydiary.databinding.ActivityLockBinding
+import com.mptsix.todaydiary.databinding.ActivityPrimaryLockBinding
 
-class LockActivity : AppCompatActivity() {
-    lateinit var binding : ActivityLockBinding
+class primaryLockActivity : AppCompatActivity() {
+    lateinit var binding : ActivityPrimaryLockBinding
     var password:String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLockBinding.inflate(layoutInflater)
+        binding = ActivityPrimaryLockBinding.inflate(layoutInflater)
         setContentView(binding.root)
         btnClicked()
         submitPwd()
-    }
-
-    override fun onBackPressed() {
-    }
-
-    private fun observePwd(){
-
     }
 
     private fun starTextInit(){
@@ -40,29 +34,15 @@ class LockActivity : AppCompatActivity() {
 
     private fun btnClicked(){
         binding.apply {
-            binding.apply {
-                var btnList = listOf<Button>(
-                    oneBtn,
-                    twoBtn,
-                    threeBtn,
-                    fourBtn,
-                    fiveBtn,
-                    sixBtn,
-                    sevenBtn,
-                    eightBtn,
-                    nineBtn,
-                    starBtn,
-                    zeroBtn,
-                    sharpBtn
-                )
-                for (btn in btnList) {
-                    btn.setOnClickListener {
-                        writePwd(btn)
-                    }
+            var btnList = listOf<Button>(oneBtn, twoBtn, threeBtn, fourBtn, fiveBtn,sixBtn,sevenBtn,eightBtn,nineBtn,starBtn, zeroBtn, sharpBtn)
+            for(btn in btnList){
+                btn.setOnClickListener {
+                    writePwd(btn)
                 }
             }
         }
     }
+
     private fun writePwd(button: Button){
         password += button.text.toString()
         starTextInit()
