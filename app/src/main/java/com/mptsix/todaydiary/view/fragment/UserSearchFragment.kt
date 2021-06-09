d16eecd4cb566bb3d5c972a3134dc49d102eff97
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-06-09 21:14:09 +0900

    Error[7] : UserSearch error handling
    findUserByUserName
    followUser
    unfollowUser

--- app/src/main/java/com/mptsix/todaydiary/view/fragment/UserSearchFragment.kt
+++ app/src/main/java/com/mptsix/todaydiary/view/fragment/UserSearchFragment.kt
@@ -1,15 +1,22 @@
 package com.mptsix.todaydiary.view.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.observe
 import com.mptsix.todaydiary.view.adapter.UserRVAdapter
 import com.mptsix.todaydiary.databinding.FragmentUserSearchBinding
 import com.mptsix.todaydiary.viewmodel.UserListViewModel
+import java.lang.RuntimeException
+import java.net.ConnectException
+import java.net.SocketTimeoutException
 
 class UserSearchFragment : SuperFragment<FragmentUserSearchBinding>() {
     private val userListViewModel : UserListViewModel by viewModels()
@@ -17,11 +24,17 @@ class UserSearchFragment : SuperFragment<FragmentUserSearchBinding>() {
 
     inner class getUserId{
         fun getFollowUserId(userId : String){
-            userListViewModel.followUser(userId)
+            userListViewModel.followUser(userId,
+            _onFailure = {
+                _onFailure(it)
+            })
         }
 
         fun getUnfollowUserId(userId : String){
-            userListViewModel.unfollowUser(userId)
+            userListViewModel.unfollowUser(userId,
+            _onFailure = {
+                _onFailure(it)
+            })
         }
     }
 
@@ -43,7 +56,19 @@ class UserSearchFragment : SuperFragment<FragmentUserSearchBinding>() {
 
         binding.searchBtn.setOnClickListener {
             var userName = binding.searchUserName.text.toString()
-            userListViewModel.findUserByUserName(userName)
+            userListViewModel.findUserByUserName(userName,
+             _onFailure = {
+                    when(it){
+                        is ConnectException, is SocketTimeoutException -> showDialog("Server Error", "서버 상태가 불안정합니다. \n잠시 후에 다시 시도해주세요.")
+                        is RuntimeException -> {
+                            showDialog("접속이 끊어졌습니다.", "로그인 페이지로 이동합니다.")
+                            // Go back login activity?
+                        }
+                        is NoSuchFieldException -> Toast.makeText(context, "검색어를 입력해주시기 바랍니다.", Toast.LENGTH_SHORT).show()
+                        else -> Toast.makeText(context, "알 수 없는 에러가 발생했습니다. 메시지: ${it.message}", Toast.LENGTH_SHORT).show()
+
+                    }
+                })
         }
     }
 }
